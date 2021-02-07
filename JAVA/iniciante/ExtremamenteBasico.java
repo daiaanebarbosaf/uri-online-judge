66d67a36c536ba820cbca4672553bb8d8c6ca79a
Daiane Barbosa Farias <daiaanebarbosaf@gmail.com>
2021-02-07 20:47:36 -0300

    Create ExtremamenteBasico.java

---
+++ JAVA/iniciante/ExtremamenteBasico.java
@@ -0,0 +1,22 @@
+package basico;
+
+import java.util.Scanner;
+
+public class ExtremamenteBasico {
+
+	public static void main(String[] args) {
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		int a = leitor.nextInt();
+		int b = leitor.nextInt();
+		
+		int x = a + b;
+		
+		System.out.println("X = " + x);
+		
+		leitor.close();
+		
+	}
+
+}
