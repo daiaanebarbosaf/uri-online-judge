39ec138d264d692b643854672232abb704282131
Daiane Barbosa Farias <daiaanebarbosaf@gmail.com>
2021-03-08 21:01:27 -0300

    Create  1059 - Números Pares.java

---
+++ JAVA/iniciante/1059 - Números Pares.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+		int contador = 2;
+		
+		while(contador <= 100) {
+			
+			System.out.println(contador);
+			contador += 2;
+		}
+  }
+}
